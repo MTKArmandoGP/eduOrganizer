68448ff983864ea793e6cfc9f77632ac738923c3
MTKArmandoGP <jsearmando767@micorreo.upp.edu.mx>
2023-03-18 10:27:51 -0600

    Splash Screen, Login y parte de Registro

---
+++ app/src/main/java/com/example/eduorganizer/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.eduorganizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Agregación de Animaciones
+        Animation animacion1= AnimationUtils.loadAnimation(this,R.anim.desplazamiento_arriba);
+        Animation animacion2= AnimationUtils.loadAnimation(this,R.anim.desplazamiento_abajo);
+
+        //Relación de parte Lógica y Gráfica
+        TextView txtBy=findViewById(R.id.tvBy);
+        TextView txtUpp=findViewById(R.id.tvUpp);
+        ImageView imgLogo=findViewById(R.id.imgLogo);
+        txtBy.setAnimation(animacion2);
+        txtUpp.setAnimation(animacion2);
+        imgLogo.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
+    }
+}
